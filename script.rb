d90b04365f55ef8bc88262b96a475f2c83828c0b
Bairdwolf <136029428+Bairdwolf@users.noreply.github.com>
2024-12-27 12:37:57 -0500

    Work with multiple words

--- script.rb
+++ script.rb
@@ -1,12 +1,16 @@
 def substrings(word, dict)
   dict.reduce(Hash.new(0)) do |output, string|
-    if word.include?(string)
-      output[string]+=1
+    word_arr=word.split(" ")
+    word_arr.each do |indiv|
+      if indiv.include?(string)
+        output[string]+=1
+      end 
     end
     output
   end
 end
 
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-puts substrings("below", dictionary)
+#dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+#puts substrings("below", dictionary)
+#puts substrings("Howdy partner, sit down! How's it going?", dictionary)
